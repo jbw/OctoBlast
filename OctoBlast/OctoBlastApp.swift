080251d5385accc95c08d4891db9ea9b0c910f32
Jason Watson <jbw@jbw.cc>
2022-08-07 18:28:31 +0100

    Initial Commit

---
+++ OctoBlast/OctoBlastApp.swift
@@ -0,0 +1,17 @@
+//
+//  OctoBlastApp.swift
+//  OctoBlast
+//
+//  Created by Jason Watson on 07/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct OctoBlastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
