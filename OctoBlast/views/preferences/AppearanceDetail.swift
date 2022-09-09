3ceb425a0ecad8d625b0b9cdc8408f28eeb9114b
Jason Watson <jbw@jbw.cc>
2022-09-09 17:40:39 +0100

    chore: move into own file

---
+++ OctoBlast/views/preferences/AppearanceDetail.swift
@@ -0,0 +1,39 @@
+//
+// Created by Jason Watson on 09/09/2022.
+// Copyright (c) 2022 JBW. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+struct AppearanceDetail: View {
+    var refreshStatusIcon: () -> Void
+
+    @State private var iconColor: Color = UserDefaults.standard.color(forKey: "iconTint")
+
+    var body: some View {
+        HStack {
+            VStack(alignment: .leading, spacing: 16) {
+                ColorPicker("Status icon color: ", selection: $iconColor, supportsOpacity: true)
+                    .onChange(
+                        of: iconColor,
+                        perform: { newValue in
+                            UserDefaults.standard.setColor(newValue, forKey: "iconTint")
+                            self.refreshStatusIcon()
+
+                        }
+                    )
+
+                Button("Reset") {
+                    UserDefaults.standard.setColor(.accentColor, forKey: "iconTint")
+                    iconColor = .accentColor
+                    self.refreshStatusIcon()
+                }
+                Spacer()
+            }
+            .padding()
+            Spacer()
+        }
+        .padding()
+    }
+}
--- OctoBlast/views/preferences/Preferences.swift
+++ OctoBlast/views/preferences/Preferences.swift
@@ -260,38 +260,6 @@ struct AdvancedDetail: View {
     }
 }
 
-struct AppearanceDetail: View {
-    var refreshStatusIcon: () -> Void
-
-    @State private var iconColor: Color = UserDefaults.standard.color(forKey: "iconTint")
-
-    var body: some View {
-        HStack {
-            VStack(alignment: .leading, spacing: 16) {
-                ColorPicker("Status icon color: ", selection: $iconColor, supportsOpacity: true)
-                    .onChange(
-                        of: iconColor,
-                        perform: { newValue in
-                            UserDefaults.standard.setColor(newValue, forKey: "iconTint")
-                            self.refreshStatusIcon()
-
-                        }
-                    )
-
-                Button("Reset") {
-                    UserDefaults.standard.setColor(.accentColor, forKey: "iconTint")
-                    iconColor = .accentColor
-                    self.refreshStatusIcon()
-                }
-                Spacer()
-            }
-            .padding()
-            Spacer()
-        }
-        .padding()
-    }
-}
-
 struct AboutDetail: View {
     @StateObject var updaterViewModel = UpdaterViewModel()
 
