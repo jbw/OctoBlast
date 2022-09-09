76d0b1c14a25f6f44636932df06f1ca36cc23a08
Jason Watson <jbw@jbw.cc>
2022-09-09 17:16:19 +0100

    refactor: split into 2 files

--- OctoBlast/lib/github/Github.swift
+++ OctoBlast/lib/github/Github.swift
@@ -4,38 +4,10 @@
 //
 //  Created by Jason Watson on 16/08/2022.
 //
+
 import Foundation
 import OctoKit
 
-class GithubOAuth {
-    public static let shared: GithubOAuth = .init()
-
-    private var token: String = Bundle.main.object(forInfoDictionaryKey: "AUTH_TOKEN") as! String
-    private var secret: String = Bundle.main.object(forInfoDictionaryKey: "AUTH_SECRET") as! String
-    private var scopes: [String] = ["read:user", "notifications"]
-
-    private var oAuthConfig: OAuthConfiguration?
-
-    public func oAuth() -> URL {
-        oAuthConfig = OAuthConfiguration(token: token, secret: secret, scopes: scopes)
-        let url = oAuthConfig!.authenticate()
-
-        return url!
-    }
-
-    public func handleOAuthCallback(url: URL, completion: @escaping (_ config: TokenConfiguration, _ user: User) -> Void) {
-        oAuthConfig!.handleOpenURL(url: url, completion: { tokenConfig in
-            Octokit(tokenConfig).me { response in
-                switch response {
-                case let .success(user):
-                    completion(tokenConfig, user)
-                case let .failure(error):
-                    print("Error: \(error)")
-                }
-            }
-        })
-    }
-}
 
 class GitHub {
     public var myNotifications: [Any] = []
