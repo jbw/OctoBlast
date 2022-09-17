00b346992222c938fb371e1b60361d8680436796
Jason Watson <hi@jbw.codes>
2022-09-17 17:00:19 +0100

    Create ApiConfig.generated.swift

---
+++ OctoBlast/templates/ApiConfig.generated.swift
@@ -0,0 +1,8 @@
+// Generated using Sourcery 1.8.2 — https://github.com/krzysztofzablocki/Sourcery
+// DO NOT EDIT
+
+
+public struct APIConfig {
+  static let authToken = ""
+  static let authSecret = ""
+}
