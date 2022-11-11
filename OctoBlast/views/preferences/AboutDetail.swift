548a5231e3f9433e81ea88e404b106fba37efe2c
Jason Watson <hi@jbw.codes>
2022-11-11 12:51:46 +0000

    refactor(about): update about view

--- OctoBlast/views/preferences/AboutDetail.swift
+++ OctoBlast/views/preferences/AboutDetail.swift
@@ -9,13 +9,40 @@ import SwiftUI
 
 struct AboutDetail: View {
     @StateObject var updaterViewModel = UpdaterViewModel()
-
+    @State private var hover: Bool = false
     var body: some View {
         VStack(alignment: .center, spacing: 16) {
             Spacer()
 
             Text("OctoBlast").font(.system(.title, design: .rounded))
 
+            Image(nsImage: NSImage(named: NSImage.Name("AppIcon"))!)
+            
+            Text("[@jasonwatson](https://twitter.com/jasonwatson) (Jason Watson)")
+                .font(.system(.body, design: .rounded))
+                .onHover { isHovered in
+                    self.hover = isHovered
+                    DispatchQueue.main.async {
+                        if (self.hover) {
+                            NSCursor.pointingHand.push()
+                        } else {
+                            NSCursor.pop()
+                        }
+                    }
+                }
+            
+            Text("[https://octoblast.jbw.codes](https://octoblast.jbw.codes)")
+                .onHover { isHovered in
+                    self.hover = isHovered
+                    DispatchQueue.main.async {
+                        if (self.hover) {
+                            NSCursor.pointingHand.push()
+                        } else {
+                            NSCursor.pop()
+                        }
+                    }
+                }
+            
             let versionLocation: URL? = Bundle.main.url(
                 forResource: "version",
                 withExtension: "txt"
