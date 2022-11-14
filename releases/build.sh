421475905a486e6633aadbb328b2aab905cf442f
Jason Watson <jwatson@breachquest.com>
2022-11-14 16:15:55 +0000

    ci: debug version

--- releases/build.sh
+++ releases/build.sh
@@ -3,6 +3,7 @@ rm -rf build
 mkdir -p releases
 
 echo ${OCTOBLAST_VERSION:-1.0.0} >OctoBlast/version.txt
+cat OctoBlast/version.txt
 sed -i .bak 's/CURRENT_PROJECT_VERSION.*;/CURRENT_PROJECT_VERSION = '${OCTOBLAST_VERSION:-1.0.0}';/g' OctoBlast.xcodeproj/project.pbxproj
 sed -i .bak 's/MARKETING_VERSION.*;/MARKETING_VERSION = '${OCTOBLAST_VERSION:-1.0.0}';/g' OctoBlast.xcodeproj/project.pbxproj
 xcodebuild clean
