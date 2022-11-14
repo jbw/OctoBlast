c4b92208031631c475e9a08058f801902bfd4bbd
Jason Watson <jwatson@breachquest.com>
2022-11-14 16:05:08 +0000

    ci: use default

--- releases/build.sh
+++ releases/build.sh
@@ -3,11 +3,11 @@ rm -rf build
 mkdir -p releases
 
 echo ${OCTOBLAST_VERSION:-1.0.0} >OctoBlast/version.txt
-sed -i .bak 's/CURRENT_PROJECT_VERSION.*;/CURRENT_PROJECT_VERSION = '$OCTOBLAST_VERSION';/g' OctoBlast.xcodeproj/project.pbxproj
-sed -i .bak 's/MARKETING_VERSION.*;/MARKETING_VERSION = '$OCTOBLAST_VERSION';/g' OctoBlast.xcodeproj/project.pbxproj
+sed -i .bak 's/CURRENT_PROJECT_VERSION.*;/CURRENT_PROJECT_VERSION = '${OCTOBLAST_VERSION:-1.0.0}';/g' OctoBlast.xcodeproj/project.pbxproj
+sed -i .bak 's/MARKETING_VERSION.*;/MARKETING_VERSION = '${OCTOBLAST_VERSION:-1.0.0}';/g' OctoBlast.xcodeproj/project.pbxproj
 xcodebuild clean
 xcodebuild clean archive -scheme OctoBlast -configuration Release -archivePath build/OctoBlast.xcarchive
 xcodebuild -exportArchive -archivePath build/OctoBlast.xcarchive -exportPath build/OctoBlast -exportOptionsPlist OctoBlast/Info.plist
 
 cd build/OctoBlast/
-zip -r ../../releases/archives/OctoBlast-$OCTOBLAST_VERSION.zip OctoBlast.app
+zip -r ../../releases/archives/OctoBlast-${OCTOBLAST_VERSION:-1.0.0}.zip OctoBlast.app
